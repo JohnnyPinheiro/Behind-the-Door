60a7bef8379087797a543346fd42c96a3a42beff
johnny pinheiro <pinheiro_dom@hotmail.com.br>
2016-01-01 18:32:30 -0200

    first commit right

---
+++ Assets/Resources/Scripts/Colisor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class Colisor : MonoBehaviour {
+
+	private bool nextLevel;
+	private bool isGround;
+	public Canvas msgComplete;
+
+	void Start(){
+		msgComplete.enabled = false;
+	}
+
+	void Update(){
+		if(nextLevel){
+	        StartCoroutine(ChangeLevel());
+	        msgComplete.enabled = true;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+		if(coll.gameObject.tag == "NextLevel"){
+			Debug.Log("bateu");
+			nextLevel = true;
+		}
+	}
+	
+	IEnumerator ChangeLevel() {
+  			yield return new WaitForSeconds(1f);
+			float fadeTime = GameObject.Find("Controller").GetComponent<Fadein>().BeginFade(1);
+			yield return new WaitForSeconds(fadeTime);
+   			Application.LoadLevel(Application.loadedLevel + 1);
+    }
+
+}
